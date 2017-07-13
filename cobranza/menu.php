dd002ae1a8dc228877d2654477d0f0affd750485
Marpez24 <ponchomarpez@gmail.com>
2017-07-13 17:31:29 -0500

    Index y menu de cobranza

---
+++ cobranza/menu.php
@@ -0,0 +1,46 @@
+<aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+          <!-- sidebar menu: : style can be found in sidebar.less -->
+          <ul class="sidebar-menu">
+            <li class="header">MENÚ PRINCIPAL</li>
+            <li>
+              <a href="index.php">
+                <i class="fa fa-home"></i> 
+                <span>Inicio</span>
+              </a>
+            </li>
+            <li class="treeview">
+                <ul class="treeview-menu">
+   
+              </ul>
+              <li>
+              <a href="index_cliente.php">
+                <i class="fa fa-file-powerpoint-o"></i>
+                <span>Proforma</span>
+              </a>
+            </li>
+            </li>
+            <li class="treeview">
+              <a href="index_usuarios.php">
+                <i class="fa fa-money"></i> 
+                <span>Cobros</span>
+              </a>
+            </li>
+            <li>
+              <a href="index_proyecto.php">
+                <i class="fa fa-bar-chart"></i>
+                <span>Proyectos vendidos</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <i class="fa fa-search"></i>
+                <span>Consultar Proforma</span>
+              </a>
+            </li>
+            
+          </ul>
+        </section>
+        <!-- /.sidebar -->
+      </aside>
