755be6acf89ded393d39b7a12ac273e4fedc21b5
Marpez24 <ponchomarpez@gmail.com>
2017-07-14 16:38:46 -0500

    Ajustes al index del administrador y inicio del modulo de usuarios, incio del breadcrumb

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Grupo Plasma Automation | Dashboard Administrador</title>
+    <title>SSP | Dashboard Administrador</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
@@ -53,10 +53,57 @@
 
             <div class="panel-heading text-center">
             <img class="profile-user-img img-responsive img-circle" src="./assets/img/gpa.png" alt="Grupo Plasma Automation">
-                <h3>Bienvenido administrador</h3>
+            <!--Inicia DashBoard
+            -->
+            <nav class="navbar navbar-default">
+              <div class="container-fluid">
+               <!-- Brand and toggle get grouped for better mobile display -->
+               <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Dash Board</a> 
+                </div>
 
-               <!-- <h4> <?php echo $_SESSION['usuario']['nombre']?></h4>-->
-    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conbranza <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Proforma</a></li>
+            <li><a href="#">Consultar Proforma</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Cobros</a></li>
+            <li><a href="#">Proyectos Vendidos</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>  
+              <div class="panel panel-danger">
+                <h3>Bienvenido Administrador</h3>
+              <!-- <h4> <?php echo $_SESSION['usuario']['nombre']?></h4>-->
+                    <h4>Sistema de seguimientos de proyectos</h4>
+                     <h6>SSP</h6>  
+                      <center><img class="img-responsive " src="./assets/img/admn.jpg" alt="Grupo Plasma Automation"></center>
             </div>
             <div class="panel-footer"></div>
           </div>
@@ -72,7 +119,7 @@
     <!-- Slimscroll -->
     <script src="assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>
     <!-- FastClick -->
-    <script src="assets/plugins/fastclick/fastclick.min.js"></script>
+    <script src="assets/plugins/fastclick/fastclick.min.js"></script>   
     <!-- AdminLTE App -->
     <script src="assets/dist/js/app.min.js"></script>
     <!-- AdminLTE for demo purposes -->
